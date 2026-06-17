42e3d6694c06bf55a94672c0ebae4712aa089b5e
Chai Vara <50233724+Chaivara@users.noreply.github.com>
2026-06-17 09:17:36 +0200

    Updates - Source fetch and Truncate load.

--- ETL/silver.py
+++ ETL/silver.py
@@ -13,7 +13,7 @@ col_names = ["icao24", "callsign", "time_position", "longitude", "latitude", "on
 cols = [0, 1, 3, 5, 6, 8, 10, 11]
 
 client = MongoClient(os.environ["MONGO_URL"])
-doc = client["airlines"]["states_all"].find_one({})
+doc = client["airlines"]["states_all"].find_one({}, sort=[("fetched_at", -1)])
 rows = [[state[i] for i in cols] for state in doc["states"]]
 
 conn = psycopg2.connect(
@@ -24,6 +24,7 @@ conn = psycopg2.connect(
     password=os.environ["SUPABASE_DB_PASSWORD"]
 )
 cur = conn.cursor()
+cur.execute("TRUNCATE TABLE map1")
 execute_values(cur,
     "INSERT INTO map1 (icao24, callsign, time_position, longitude, latitude, on_ground, true_track, vertical_rate, created_at) VALUES %s",
     [[*row, datetime.now(timezone.utc)] for row in rows]
